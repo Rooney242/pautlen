e9fa88fb73c3c26f2f8d997b4e733931f1325212
ggallego2 <guillermo.gallego@gmail.com>
2018-10-04 15:09:23 +0200

    funciones principales terminadas

--- lib/generacion.c
+++ lib/generacion.c
@@ -5,7 +5,8 @@ void escribir_cabecera_bss(FILE* fpasm){
 	fprintf(fpasm,"segment.bss\n\t__esp resd 1\n");
 }
 void escribir_subseccion_data(FILE* fpasm){
-	fprintf(fpasm,"segment .data\n");
+	//Con mensaje de error de division cero
+	fprintf(fpasm,"segment .data\n\tmsg_div_cero\tdb \"ERROR: DIVISOR ES CERO\", 0");
 }
 void declarar_variable(FILE* fpasm, char * nombre,  int tipo,  int tamano){
 	fprintf(fpasm,"/t_%s resd %d\n",nombre,tamano);
@@ -13,16 +14,38 @@ void declarar_variable(FILE* fpasm, char * nombre,  int tipo,  int tamano){
 
 void escribir_segmento_codigo(FILE* fpasm){
 	fprintf(fpasm,"segment .text\n\tglobal main\n\textern scan_int, print_int, scan_float, print_float," 
-	"scan_boolean, print_boolean\n\textern print_endofline, print_blank, print_string\n\textern alfa_malloc, alfa_free, ld_float");
+	"scan_boolean, print_boolean\n\textern print_endofline, print_blank, print_string\n\textern alfa_malloc, alfa_free, ld_float\n");
 }
 void escribir_inicio_main(FILE* fpasm){
-	fprintf(fpasm,"; -----------------------\n; PROCEDIMIENTO PRINCIPAL\n; -----------------------\nmain:\n\tmov dword [__esp] , esp");
+	fprintf(fpasm,"; -----------------------\n; PROCEDIMIENTO PRINCIPAL\n; -----------------------\nmain:\n\tmov dword [__esp] , esp\n");
 }
 void escribir_fin(FILE* fpasm){
 	fprintf(fpasm,"fin:\n\tmov dword esp, [__esp]\n\tret");
 }
 void escribir_operando(FILE* fpasm, char* nombre, int es_variable){
-	fprintf(fpasm,"")
+	//Si es una variable se escribe con los corchetes de direccion
+	if (es_variable == 1)
+		fprintf(fpasm,"\tpush dword [_%s]\n",nombre);
+	//Si no, se escibe tal como esta
+	if(es_variable == 0)
+		fprintf(fpasm,"\tpush dword %s\n",nombre);
+}
+
+void asignar(FILE* fpasm, char* nombre, int es_variable){
+	// Comprobamos si los operandos son referencias o valores explicitos
+	if (es_variable == 1){
+		//Sacamos la variable de pila
+		fprintf(fpasm, "\tpop dword eax\n");
+		//La asignamos a la referencia
+		fprintf(fpasm, "\tmov dword [_%s] , [eax]", nombre);
+	}
+	if (es_variable == 0){
+		//Sacamos la variable de pila
+		fprintf(fpasm, "\tpop dword eax\n");
+		//La asignamos a la referencia
+		fprintf(fpasm, "\tmov dword [_%s] , eax", nombre);
+	}
+	
 }
 
 
