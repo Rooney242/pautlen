f1b42b77dc964e0a67799a796fe6dbffca7d8352
Tu Nombre <you@example.com>
2018-11-07 17:54:42 +0100

    a sublime

---
+++ proyecto_pautlen/PP/P4/prueba_ts.c
@@ -0,0 +1,39 @@
+#define TOK_OP_INICIA_TSA_MAIN        "inicia_tsa_main"
+#define TOK_OP_ABRIR_AMBITO_PPAL_MAIN    "abrir_ambito_ppal_main"
+#define TOK_OP_BUSCAR            "buscar"
+#define TOK_OP_INSERTAR_TSA_MAIN     "insertar_tsa_main"
+#define TOK_OP_ABRIR_AMBITO_TSA_MAIN    "abrir_ambito_tsa_main"
+#define TOK_OP_CERRAR_AMBITO_TSA_MAIN    "cerrar_ambito_tsa_main"
+#define TOK_OP_INICIA_TSC         "inicia_tsc"
+		"abrir_clase"
+		abrir_clase_hereda
+		insertar_tsc
+		abrir_ambito_tsc
+		cerrar_ambito_tsc
+#define TOK_OP_CERRAR_CLASE        "cerrar_clase"
+#define TOK_OP_CERRAR_TSA_MAIN        "cerrar_tsa_main"
+#define TOK_OP_CERRAR_TSC        "cerrar_tsc"
+
+// PARAMETROS
+#define TOK_DECLARAR_MAIN        "declarar_main"
+#define TOK_DECLARAR_MIEMBRO_CLASE    "declarar_miembro_clase"
+#define TOK_DECLARAR_MIEMBRO_INSTANCIA    "declarar_miembro_instancia"
+#define TOK_NOMBRE_ID	"nombre_id" 
+nombre_clase_desde 
+nombre_miembro
+declarar_id_local_metodo
+nombre_clase
+jerarquia
+id_no_cualificado
+nombre_instancia
+nombre_clase_cualifica
+categoria
+tipo_basico
+estructura
+tipo_acceso
+tipo_miembro
+nombre_ambito
+nombre_padre
+#define TOK_ID_CUALIFICADO_INSTANCIA    "id_cualificado_instancia"
+#define TOK_ID_CUALIFICADO_CLASE    "id_cualificado_clase"
+
