4bdc52ad4ee9efceea022335dddcf3058ff6a2fe
Jesús Miguel Álvarez <suso1996@hotmail.com>
2018-11-07 17:05:11 +0100

    Sublime done

--- proyecto_pautlen/PP/P4/prueba_ts.c
+++ proyecto_pautlen/PP/P4/prueba_ts.c
@@ -1,39 +1,39 @@
-#define TOK_OP_INICIA_TSA_MAIN        "inicia_tsa_main"
-#define TOK_OP_ABRIR_AMBITO_PPAL_MAIN    "abrir_ambito_ppal_main"
-#define TOK_OP_BUSCAR            "buscar"
-#define TOK_OP_INSERTAR_TSA_MAIN     "insertar_tsa_main"
+#define TOK_OP_INICIA_TSA_MAIN        	"inicia_tsa_main"
+#define TOK_OP_ABRIR_AMBITO_PPAL_MAIN   "abrir_ambito_ppal_main"
+#define TOK_OP_BUSCAR            		"buscar"
+#define TOK_OP_INSERTAR_TSA_MAIN     	"insertar_tsa_main"
 #define TOK_OP_ABRIR_AMBITO_TSA_MAIN    "abrir_ambito_tsa_main"
-#define TOK_OP_CERRAR_AMBITO_TSA_MAIN    "cerrar_ambito_tsa_main"
-#define TOK_OP_INICIA_TSC         "inicia_tsc"
-		"abrir_clase"
-		abrir_clase_hereda
-		insertar_tsc
-		abrir_ambito_tsc
-		cerrar_ambito_tsc
-#define TOK_OP_CERRAR_CLASE        "cerrar_clase"
-#define TOK_OP_CERRAR_TSA_MAIN        "cerrar_tsa_main"
-#define TOK_OP_CERRAR_TSC        "cerrar_tsc"
+#define TOK_OP_CERRAR_AMBITO_TSA_MAIN	"cerrar_ambito_tsa_main"
+#define TOK_OP_INICIA_TSC         		"inicia_tsc"
+#define TOK_OP_ABRIR_CLASE 				"abrir_clase"
+#define TOK_OP_ABRIR_CLASE_HEREDA 		"abrir_clase_hereda"
+#define TOK_OP_INSERTAR_TSC 			"insertar_tsc"
+#define TOK_OP_ABRIR_AMBITO_TSC 		"abrir_ambito_tsc"
+#define TOK_OP_CERRAR_AMBITO_TSC 		"cerrar_ambito_tsc"
+#define TOK_OP_CERRAR_CLASE        		"cerrar_clase"
+#define TOK_OP_CERRAR_TSA_MAIN        	"cerrar_tsa_main"
+#define TOK_OP_CERRAR_TSC        		"cerrar_tsc"
 
 // PARAMETROS
-#define TOK_DECLARAR_MAIN        "declarar_main"
-#define TOK_DECLARAR_MIEMBRO_CLASE    "declarar_miembro_clase"
-#define TOK_DECLARAR_MIEMBRO_INSTANCIA    "declarar_miembro_instancia"
-#define TOK_NOMBRE_ID	"nombre_id" 
-nombre_clase_desde 
-nombre_miembro
-declarar_id_local_metodo
-nombre_clase
-jerarquia
-id_no_cualificado
-nombre_instancia
-nombre_clase_cualifica
-categoria
-tipo_basico
-estructura
-tipo_acceso
-tipo_miembro
-nombre_ambito
-nombre_padre
+#define TOK_DECLARAR_MAIN        		"declarar_main"
+#define TOK_DECLARAR_MIEMBRO_CLASE    	"declarar_miembro_clase"
+#define TOK_DECLARAR_MIEMBRO_INSTANCIA	"declarar_miembro_instancia"
+#define TOK_NOMBRE_ID					"nombre_id" 
+#define TOK_NOMBRE_CLASE_DESDE 			"nombre_clase_desde"
+#define TOK_NOMBRE_MIEMBRO 				"nombre_miembro"
+#define TOK_declarar_id_local_metodo	"declarar_id_local_metodo"
+#define TOK_NOMBRE_CLASE 				"nombre_clase"
+#define TOK_JERARQUIA 					"jerarquia"
+#define TOK_ID_NO_CUALIFICADO 			"id_no_cualificado"
+#define TOK_NOMBRE_INSTANCIA 			"nombre_instancia"
+#define TOK_NOMBRE_CLASE_CUALIFICA		"nombre_clase_cualifica"
+#define TOK_CATEGORIA					"categoria"
+#define TOK_TIPO_BASICO 				"tipo_basico"
+#define TOK_ESTRUCTURA					"estructura"
+#define TOK_TIPO_ACCESO					"tipo_acceso"
+#define TOK_TIPO_MIEMBRO 				"tipo_miembro"
+#define TOK_NOMBRE_AMBITO 				"nombre_ambito"
+#define TOK_NOMBRE_PADRE 				"nombre_padre"
 #define TOK_ID_CUALIFICADO_INSTANCIA    "id_cualificado_instancia"
-#define TOK_ID_CUALIFICADO_CLASE    "id_cualificado_clase"
+#define TOK_ID_CUALIFICADO_CLASE    	"id_cualificado_clase"
 
