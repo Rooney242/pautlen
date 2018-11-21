45f43cc62f49cfdb65dab014105ccc00c1833326
roni <david.morenomaldonado@estudiante.uam.es>
2018-11-22 00:26:23 +0100

    nunca crei que diria esto, pero estoy viendo ot mientras programo paulen

--- proyecto_pautlen/OO/includes/casos.h
+++ proyecto_pautlen/OO/includes/casos.h
@@ -4,43 +4,43 @@
 #include "tsa.h"
 #include "hash_table.h"
 
-#define ERROR_20 -20/*v7 BUSCAR ID NO CUALIFICADO*/
-#define ERROR_22 -22/*v7 BUSCAR ID NO CUALIFICADO*/
-#define ERROR_23 -23/*v7 BUSCAR ID NO CUALIFICADO*/
-#define ERROR_21 -21/*v7 BUSCAR ID NO CUALIFICADO*/
-#define ERROR_24 -24/*v7 BUSCAR ID NO CUALIFICADO*/
-#define ERROR_25 -25/*v7 BUSCAR ID NO CUALIFICADO*/
-#define ERROR_26 -26/*v7 BUSCAR ID NO CUALIFICADO*///////////////////////////////////////////////
-#define ERROR_27 -27/*v8*/
-#define ERROR_28 -28/*v8*/
-#define ERROR_29 -29/*v8*/
-#define ERROR_30 -30/*v8*/
-#define ERROR_31 -31/*v8*/
-#define ERROR_32 -32/*v8*/
-#define ERROR_33 -33/*v8*/
-#define ERROR_34 -34/*v8*/
-#define ERROR_35 -35/*v8*/
-#define ERROR_36 -36/*v8*/
-#define ERROR_37 -37/*v8*/
-#define ERROR_38 -38/*v8*/
-#define ERROR_39 -39/*v8*/
-#define ERROR_40 -40/*v8*/
+#define CASO_20 -20/*v7 BUSCAR ID NO CUALIFICADO*/
+#define CASO_22 -22/*v7 BUSCAR ID NO CUALIFICADO*/
+#define CASO_23 -23/*v7 BUSCAR ID NO CUALIFICADO*/
+#define CASO_21 -21/*v7 BUSCAR ID NO CUALIFICADO*/
+#define CASO_24 -24/*v7 BUSCAR ID NO CUALIFICADO*/
+#define CASO_25 -25/*v7 BUSCAR ID NO CUALIFICADO*/
+#define CASO_26 -26/*v7 BUSCAR ID NO CUALIFICADO*///////////////////////////////////////////////
+#define CASO_27 -27/*v8*/
+#define CASO_28 -28/*v8*/
+#define CASO_29 -29/*v8*/
+#define CASO_30 -30/*v8*/
+#define CASO_31 -31/*v8*/
+#define CASO_32 -32/*v8*/
+#define CASO_33 -33/*v8*/
+#define CASO_34 -34/*v8*/
+#define CASO_35 -35/*v8*/
+#define CASO_36 -36/*v8*/
+#define CASO_37 -37/*v8*/
+#define CASO_38 -38/*v8*/
+#define CASO_39 -39/*v8*/
+#define CASO_40 -40/*v8*/
 
-#define ERROR_42 -42/*v8*/
-#define ERROR_43 -43/*v8*/
+#define CASO_42 -42/*v8*/
+#define CASO_43 -43/*v8*/
 
-#define ERROR_45 -45/*v8*/
-#define ERROR_46 -46/*v8*/
-#define ERROR_47 -47/*v8*/
+#define CASO_45 -45/*v8*/
+#define CASO_46 -46/*v8*/
+#define CASO_47 -47/*v8*/
 
-#define ERROR_49 -49/*v8 BUSCAR PARA DECLARAR MIEMBRO CLASE*/
-#define ERROR_50 -50/*v8 BUSCAR PARA DECLARAR MIEMBRO CLASE*/
-#define ERROR_51 -51/*v8 BUSCAR PARA DECLARAR MIEMBRO INSTANCIA*/
-#define ERROR_52 -52/*v8 BUSCAR PARA DECLARAR MIEMBRO INSTANCIA*/
-#define ERROR_53 -53/*v8 BUSCAR PARA DECLARAR MIEMBRO INSTANCIA*/
+#define CASO_49 -49/*v8 BUSCAR PARA DECLARAR MIEMBRO CLASE*/
+#define CASO_50 -50/*v8 BUSCAR PARA DECLARAR MIEMBRO CLASE*/
+#define CASO_51 -51/*v8 BUSCAR PARA DECLARAR MIEMBRO INSTANCIA*/
+#define CASO_52 -52/*v8 BUSCAR PARA DECLARAR MIEMBRO INSTANCIA*/
+#define CASO_53 -53/*v8 BUSCAR PARA DECLARAR MIEMBRO INSTANCIA*/
 
 
 
-int	print_error(FILE* pf, int error, char* id, char* ambito_desde, tsa* ambito_encontrado, tsa_elem* elem);
+int	print_caso(FILE* pf, int caso, char* id, char* ambito_desde, tsa* ambito_encontrado, tsa_elem* elem);
 
 #endif
--- proyecto_pautlen/OO/src/casos.c
+++ proyecto_pautlen/OO/src/casos.c
@@ -8,68 +8,65 @@
 #include "casos.h"
 
 
-int	print_error(FILE* pf, int error, char* id, char* ambito_desde, tsa* ambito_encontrado, tsa_elem* elem){
-	if(!pf) return ERROR;
+int	print_caso(FILE* pf, int caso, char* id, char* ambito_desde, tsa* ambito_encontrado, tsa_elem* elem){
+	if(!pf) return CASO;
 
-	switch(error){
+	switch(caso){
 		case ERROR:
-			fprintf(pf, "HA HABIDO UN ERROR.\n");
-			break;
-		case ERROR_20:
 			fprintf(pf, "CASO 20 -- OK\n"
 				"-->Elemento %s en buscarIdNoCualificado desde (%s) ENCONTRADO en main.\n", id, ambito_desde);
 			break;
-		case ERROR_21:
+		case CASO_21:
 			fprintf(pf, "CASO 21 -- ERR\n"
 				"-->FALLO en la busqueda del id %s en buscarIdNoCualificado desde %s \n", id, ambito_desde);
 			break;
-		case ERROR_22:
+		case CASO_22:
 			fprintf(pf, "CASO 22 -- OK\n"
 				"-->Elemento %s en buscarIdNoCualificado desde %s ENCONTRADO en main\n", id, ambito_desde);
 			break;
-		case ERROR_23:
+		case CASO_23:
 			fprintf(pf, "CASO 23 -- ERR\n"
 				"-->FALLO en la busqueda del id %s en buscarIdNoCualificado desde %s\n", id, ambito_desde);
 			break;
-		case ERROR_24:
+		case CASO_24:
 			fprintf(pf, "CASO 24 -- OK\n"
 				"-->Elemento %s en buscarIdNoCualificado desde %s ENCONTRADO en %s\n", id, ambito_desde, ambito_encontrado->ambito);
 			break;
-		case ERROR_25:
+		case CASO_25:
 			fprintf(pf, "CASO 25 -- OK\n"
 				"-->Elemento %s en buscarIdNoCualificado desde %s ENCONTRADO en main\n", id, ambito_desde);
 			break; 
-		case ERROR_26:
+		case CASO_26:
 			fprintf(pf, "CASO 26 -- ERR\n"
 				"-->FALLO en la busqueda del id %s en buscarIdNoCualificado desde %s\n",id, ambito_desde);
 			break;
 
-		case ERROR_49:
+		case CASO_49:
 			fprintf(pf, "CASO 49\n"
-				"BUSCAR PARA DECLARAR MIEMBRO CLASE: ERROR %s NO ESTA",id);
+				"BUSCAR PARA DECLARAR MIEMBRO CLASE: CASO %s NO ESTA",id);
 			break; 
-		case ERROR_50:
+		case CASO_50:
 			fprintf(pf, "CASO 50\n"
 				"BUSCAR PARA DECLARAR MIEMBRO CLASE: OK %s YA ESTA",id);
 			break;
 
 
 
-		case ERROR_51:
+		case CASO_51:
 			fprintf(pf, "CASO 51\n"
 				"BUSCAR PARA DECLARAR MIEMBRO INSTANCIA: OK %s NO SE PUEDE DECLARAR",id);
 			break; 
-		case ERROR_52:
+		case CASO_52:
 			fprintf(pf, "CASO 52\n"
 				"BUSCAR PARA DECLARAR MIEMBRO INSTANCIA: NO SE HACERLO HULIO");
 //DECLARADO EN AA ES ATRIBUTO DE INSTANCIA ACCESIBLE CON ACCESO secret NO
 //EXISTE AA_ha1 (EN AA) PERO NO ES ACCESIBLE PORQUE ES HIDDEN SE PUEDE DECLARAR	
 //EXISTE EN LA JERARQUIA (AA) ES ACCESIBLE (SECRET) Y ES SOBREESCRIBIBLE: SE ESTÁ SOBREESCRIBIENDO Y EL OFFSET ACUMULADO PAR LA TABLA DE MÉTODOS ES EL MISMO QUE EL DE AA_mA1@1
-//ERROR (AA_mA2) mA2 EXISTE EN AA PERO NO ES ACCESIBLE (HIDDEN) SE PEUDE DECLARAR COMO UN MÉTODO SOBRERESCRBIBLE NUVO CON SU PROPIO OFFSET ACUMULADO PARA LA TABLA DE METODOS
+//CASO (AA_mA2) mA2 EXISTE EN AA PERO NO ES ACCESIBLE (HIDDEN) SE PEUDE DECLARAR COMO UN MÉTODO SOBRERESCRBIBLE NUVO CON SU PROPIO OFFSET ACUMULADO PARA LA TABLA DE METODOS
 			break; 
-		case ERROR_53:
+		case CASO_53:
 			fprintf(pf, "CASO 53\n"
-				"BUSCAR PARA DECLARAR MIEMBRO INSTANCIA: ERROR %s SE PUEDE DECLARAR",id);
+				"BUSCAR PARA DECLARAR MIEMBRO INSTANCIA: CASO %s SE PUEDE DECLARAR",id);
 			break;
 
 
